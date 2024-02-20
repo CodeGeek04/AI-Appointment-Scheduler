8f31b897a3929768f3abd3237f3414aca6c89bae
abhishek kumar <abhishek21k15@gmail.com>
2024-02-20 12:49:31 +0530

    final routes

---
+++ src/app/api/addMessage/route.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { NextResponse, type NextRequest } from "next/server";
+import { z } from "zod";
+import { db } from "~/server/db";
+
+
+const RegisterRequestBody = z.object({
+    mobile: z.string().min(5).max(15),
+    message: z.string().default(""),
+
+});
+export async function POST(request: NextRequest) {
+    const req = await request.json();
+    const body = RegisterRequestBody.parse(req);
+    try {
+
+        const user = await db.messages.create({
+            data: {
+                mobile: body.mobile,
+                message: body.message,
+            },
+        });
+        return NextResponse.json({ message: user, status: 200 });
+    }
+
+    catch (e: any) {
+        if (e.code == "P2002") {
+            return NextResponse.json({
+                message: "ERROR ADDING MESSAGE",
+                status: 400,
+            });
+        }
+        return NextResponse.json({ message: e, status: 500 });
+    }
+}
